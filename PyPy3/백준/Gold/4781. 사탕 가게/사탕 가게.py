e1e7e140b08f94c6e463b4e86edb96a8d2c5762c
Choi JinWoo <cjw1234577@gmail.com>
2024-03-22 23:38:45 +0900

    [Gold IV] Title: 사탕 가게, Time: 1552 ms, Memory: 111104 KB -BaekjoonHub

--- PyPy3/백준/Gold/4781. 사탕 가게/사탕 가게.py
+++ PyPy3/백준/Gold/4781. 사탕 가게/사탕 가게.py
@@ -1,23 +1,22 @@
-while True:
-    candy = []
+n, m = input().split()
+n, m = int(n), float(m)
+m*=100
+m+=0.5
+m = int(m)
+
+while n != 0 or m != 0:
     dp = [0] * 10001
-    n, m = input().split()
-    n, m = int(n), float(m)
-    m *= 100
-    m+=0.5
-    m = int(m)
-    if n == 0 and m == 0:
-        break
     for i in range(n):
         a, b = input().split()
         a, b = int(a), float(b)
         b*=100
         b+=0.5
         b = int(b)
-        candy.append((a, b))
-        
-    for i in range(1, m + 1):
-        for a, b in candy:
-            if i - b >= 0:
-                dp[i] = max(dp[i], dp[i - b] + a)
+        for j in range(b, m + 1):
+            dp[j] = max(dp[j], dp[j - b] + a)
     print(dp[m])
+    n, m = input().split()
+    n, m = int(n), float(m)
+    m *= 100
+    m+=0.5
+    m = int(m)
