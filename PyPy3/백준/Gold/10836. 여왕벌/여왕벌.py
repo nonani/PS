4d208e7d4cacd037845e6ec74d280a47e8d9f596
Choi JinWoo <cjw1234577@gmail.com>
2024-03-25 23:48:27 +0900

    [Gold IV] Title: 여왕벌, Time: 292 ms, Memory: 116428 KB, Score: 83 point -BaekjoonHub

--- PyPy3/백준/Gold/10836. 여왕벌/여왕벌.py
+++ PyPy3/백준/Gold/10836. 여왕벌/여왕벌.py
@@ -1,42 +1,26 @@
 import sys
 input = sys.stdin.readline
-M, N = map(int, input().rstrip().rsplit())
+M, N = map(int, input().split())
 board = [[1] * M for _ in range(M)]
 
 for _ in range(N):
-    grow = list(map(int, input().split()))
-    day_grow = [[0] * M for _ in range(M)]
-    for i in range(M-1, -1, -1):
-        if grow[0] > 0:
-            day_grow[i][0] = 0
-            grow[0] -= 1
-        elif grow[1] > 0:
-            day_grow[i][0] = 1
-            grow[1] -= 1
-        elif grow[2] > 0:
-            day_grow[i][0] = 2
-            grow[2] -= 1
-    for i in range(1, M):
-        if grow[0] > 0:
-            day_grow[0][i] = 0
-            grow[0] -= 1
-        elif grow[1] > 0:
-            day_grow[0][i] = 1
-            grow[1] -= 1
-        elif grow[2] > 0:
-            day_grow[0][i] = 2
-            grow[2] -= 1
-    for i in range(0, M):
-        for j in range(0, M):
-            if i == 0 or j == 0:
-                board[i][j] += day_grow[i][j]
-                continue
-            day_grow[i][j] = max(day_grow[i-1][j-1], max(day_grow[i-1][j], day_grow[i][j-1]))
-            board[i][j] += day_grow[i][j]
-
-for i in range(M):
-    for j in range(M):
-        if j == M-1:
-            print(board[i][j])
-        else:
-            print(board[i][j], end=' ')
+    a,b,c = map(int, input().split())
+    grow = [0]*(2*M-1)
+    for idx in range(a,a+b):
+        grow[idx] += 1
+    for idx in range(a+b,2*M-1):
+        grow[idx] += 2
+    for i in range(M) :
+        board[-1 - i][0] += grow[i]
+    for i in range(M, 2*M-1) :
+        board[0][i+1-M] += grow[i]
+for i in range(M) :
+    for j in range(M) :
+        if i == 0 or j == 0 :
+            if j == M-1 :
+                print(board[i][j])    
+            else:
+                print(board[i][j], end = ' ')
+        elif j != M-1 :
+            print(board[0][j], end = ' ')
+        else : print(board[0][j])
